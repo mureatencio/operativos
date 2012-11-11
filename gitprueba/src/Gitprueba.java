11ef0327b58488075874f7db6a72dd88ffe23214
mureatencio <mureatencio@gmail.com>
2012-11-11 01:11:28 -0600

    Mensajito de holamundo

---
+++ gitprueba/src/Gitprueba.java
@@ -0,0 +1,20 @@
+import java.io.*;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel
+ */
+public class Gitprueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hola mundo");
+        // TODO code application logic here
+    }
+}
